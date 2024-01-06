d89ce37f617ed23b4faeaca666fc0eb27782e245
Rawand Ahmed Shaswar <rawandahmad698@gmail.com>
2024-01-06 16:59:50 +0300

    fix workflow bruh

--- noble_tls/__version__.py
+++ noble_tls/__version__.py
@@ -1,5 +1,5 @@
 __title__ = "Noble TLS"
 __description__ = "Advanced TLS/SSL wrapper for Python"
-__version__ = "0.0.85"
+__version__ = "0.0.86"
 __author__ = "Rawand Ahmed Shaswar"
 __license__ = "MIT"
