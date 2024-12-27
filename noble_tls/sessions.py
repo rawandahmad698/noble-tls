1962f4f7e09b2f6586c04ffd6674847b7524227e
Evgeny <evgeny@zyte.com>
2024-12-27 11:28:40 +0300

    Add sessionDestroy from shared lib

--- noble_tls/sessions.py
+++ noble_tls/sessions.py
@@ -6,7 +6,7 @@ import urllib.parse
 import base64
 import ctypes
 
-from .c.cffi import request, free_memory
+from .c.cffi import request, free_memory, destroy_session
 from .cookies import cookiejar_from_dict, merge_cookies, extract_cookies_to_jar
 from .exceptions.exceptions import TLSClientException
 from .utils.structures import CaseInsensitiveDict
@@ -513,3 +513,16 @@ class Session:
     ):
         """Sends a DELETE request"""
         return await self.execute_request(method="DELETE", url=url, **kwargs)
+
+    def destroy_session(self):
+        destroy_session_payload = {
+            "sessionId": self._session_id,
+        }
+        # should this be async?
+        destroy_session_response = destroy_session(json.dumps(destroy_session_payload).encode('utf-8'))
+
+        destroy_session_response_bytes = ctypes.string_at(destroy_session_response)
+        destroy_session_response_string = destroy_session_response_bytes.decode('utf-8')
+        destroy_session_response_object = json.loads(destroy_session_response_string)
+
+        return destroy_session_response_object
