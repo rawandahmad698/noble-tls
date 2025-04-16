03b0387816757cb70ce56ff4ed8969c908e6cdab
ZAKI Omar <omar6995@hotmail.com>
2025-04-17 00:39:35 +0200

    Update __version__.py

--- noble_tls/__version__.py
+++ noble_tls/__version__.py
@@ -1,5 +1,5 @@
-__title__ = "noble_tls"
+__title__ = "Forked-nobletls	"
 __description__ = "Advanced TLS/SSL wrapper for Python"
 __version__ = "0.0.4"
-__author__ = "Rawand Ahmed Shaswar"
+__author__ = "Omar Z"
 __license__ = "MIT"
